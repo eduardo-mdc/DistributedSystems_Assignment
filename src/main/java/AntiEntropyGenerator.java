ab18ad4c13838b1cc4cdf95c40857f424cd1ea56
Eduardo Correia <eduardo.mmd.correia@gmail.com>
2023-11-29 17:38:02 +0000

    functioning json mapper

--- src/main/java/AntiEntropyGenerator.java
+++ src/main/java/AntiEntropyGenerator.java
@@ -7,13 +7,13 @@ import java.util.Map;
 
 public class AntiEntropyGenerator {
     public static void main(String[] args) {
-        Map<Integer, List<SocketIdentifier>> peerMap = AntiPeerMapper.map();
+        Map<Integer, List<SocketIdentifier>> peerMap = AntiPeerMapper.readPeerMapFromJson();
 
         for (Map.Entry<Integer, List<SocketIdentifier>> entry : peerMap.entrySet()) {
             SocketIdentifier currentPeer = new SocketIdentifier("localhost",entry.getKey());
             List<SocketIdentifier> neighborPeers = entry.getValue();
 
-            //new AntiEntropyPeer(currentPeer, neighborPeers).start();
+            new AntiEntropyPeer(currentPeer, neighborPeers).start();
         }
     }
 
--- src/main/java/entropy/AntiPeerMapper.java
+++ src/main/java/entropy/AntiPeerMapper.java
@@ -1,12 +1,13 @@
 package entropy;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import general_utils.SocketIdentifier;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,30 +16,81 @@ import java.util.Map;
 public class AntiPeerMapper {
     private static final String JSON_FILE_PATH = "config/entropyMapping.json";
 
-    static {
-        // Register the SocketIdentifierDeserializer with the ObjectMapper
-        ObjectMapper objectMapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(SocketIdentifier.class, new SocketIdentifierDeserializer());
-        objectMapper.registerModule(module);
+    public static Map<Integer, List<SocketIdentifier>> readPeerMapFromJson() {
+        try {
+            String jsonContent = new String(Files.readAllBytes(Paths.get(JSON_FILE_PATH)));
+            JSONObject jsonMap = new JSONObject(jsonContent);
+            return parseJsonToPeerMap(jsonMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
     }
 
-    public static Map<Integer, List<SocketIdentifier>> map() {
-        Map<Integer, List<SocketIdentifier>> peerMap = new HashMap<>();
-        ObjectMapper mapper = new ObjectMapper();
-
+    public static void writePeerMapToJson(Map<Integer, List<SocketIdentifier>> peerMap) {
+        JSONObject jsonMap = createJsonFromPeerMap(peerMap);
         try {
-            File jsonFile = new File(JSON_FILE_PATH);
-            if (!jsonFile.exists()) {
-                // Create the file with the default mapping
-                mapper.writeValue(jsonFile, peerMap);
-            } else {
-                // Load the data from the existing file
-                peerMap = mapper.readValue(jsonFile, new TypeReference<HashMap<Integer, List<SocketIdentifier>>>() {});
-            }
+            Files.write(Paths.get(JSON_FILE_PATH), jsonMap.toString(4).getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Map<Integer, List<SocketIdentifier>> parseJsonToPeerMap(JSONObject jsonMap) {
+        Map<Integer, List<SocketIdentifier>> peerMap = new HashMap<>();
+        for (String key : jsonMap.keySet()) {
+            int port = Integer.parseInt(key);
+            JSONArray socketArray = jsonMap.getJSONArray(key);
+            List<SocketIdentifier> socketList = new ArrayList<>();
+            for (int i = 0; i < socketArray.length(); i++) {
+                JSONObject socketJson = socketArray.getJSONObject(i);
+                String host = socketJson.getString("host");
+                int portValue = socketJson.getInt("port");
+                socketList.add(new SocketIdentifier(host, portValue));
+            }
+            peerMap.put(port, socketList);
+        }
+        return peerMap;
+    }
+
+    private static JSONObject createJsonFromPeerMap(Map<Integer, List<SocketIdentifier>> peerMap) {
+        JSONObject jsonMap = new JSONObject();
+        for (Map.Entry<Integer, List<SocketIdentifier>> entry : peerMap.entrySet()) {
+            int port = entry.getKey();
+            List<SocketIdentifier> socketList = entry.getValue();
+            JSONArray socketArray = new JSONArray();
+            for (SocketIdentifier socketIdentifier : socketList) {
+                JSONObject socketJson = new JSONObject();
+                socketJson.put("host", socketIdentifier.getHostname());
+                socketJson.put("port", socketIdentifier.getPort());
+                socketArray.put(socketJson);
+            }
+            jsonMap.put(Integer.toString(port), socketArray);
+        }
+        return jsonMap;
+    }
+
+    public static void main(String[] args) {
+        // Create or read the peerMap from/to JSON
+        Map<Integer, List<SocketIdentifier>> peerMap = map();
+        writePeerMapToJson(peerMap);
+
+        // Read the peerMap from JSON
+        Map<Integer, List<SocketIdentifier>> readPeerMap = readPeerMapFromJson();
+
+        // Use the readPeerMap as needed
+        System.out.println("Read Peer Map: " + readPeerMap);
+    }
+
+    public static Map<Integer, List<SocketIdentifier>> map() {
+        // Your existing mapping logic
+        Map<Integer, List<SocketIdentifier>> peerMap = new HashMap<>();
+        peerMap.put(5001, generateSocketList(new Integer[]{5002}));
+        peerMap.put(5002, generateSocketList(new Integer[]{5003, 5004}));
+        peerMap.put(5003, generateSocketList(new Integer[]{5002}));
+        peerMap.put(5004, generateSocketList(new Integer[]{5002, 5005, 5006}));
+        peerMap.put(5005, generateSocketList(new Integer[]{5004}));
+        peerMap.put(5006, generateSocketList(new Integer[]{5004}));
 
         return peerMap;
     }
@@ -48,16 +100,5 @@ public class AntiPeerMapper {
         for (Integer number : numbers)
             socketList.add(new SocketIdentifier("localhost", number));
         return socketList;
-    }
-
-    // Inner class for deserializing SocketIdentifier
-    private static class SocketIdentifierDeserializer extends com.fasterxml.jackson.databind.JsonDeserializer<SocketIdentifier> {
-        @Override
-        public SocketIdentifier deserialize(com.fasterxml.jackson.core.JsonParser jsonParser, com.fasterxml.jackson.databind.DeserializationContext deserializationContext) throws IOException, com.fasterxml.jackson.core.JsonProcessingException {
-            com.fasterxml.jackson.databind.JsonNode jsonNode = jsonParser.getCodec().readTree(jsonParser);
-            String hostname = jsonNode.get("hostname").asText();
-            int port = jsonNode.get("port").asInt();
-            return new SocketIdentifier(hostname, port);
-        }
     }
 }

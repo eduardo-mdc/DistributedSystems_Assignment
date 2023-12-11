b8d9b5afe0f5e3090379dc0ba35cdd582a2bbed0
Eduardo Correia <eduardo.mmd.correia@gmail.com>
2023-12-11 18:21:51 +0000

    finished task 2

--- src/main/java/entropy/server/PeerServerImplementation.java
+++ src/main/java/entropy/server/PeerServerImplementation.java
@@ -10,6 +10,7 @@ import io.grpc.stub.StreamObserver;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -19,20 +20,26 @@ public class PeerServerImplementation extends EntropyPeerServiceGrpc.EntropyPeer
     Logger logger;
 
     List<Double> server_values;
+    private Thread numberGeneratorThread;
+
 
     public PeerServerImplementation(SocketIdentifier thisPeer, List<SocketIdentifier> neighbours, Logger logger){
         this.thisPeer = thisPeer;
         this.neighbours = neighbours;
         this.logger = logger;
-        server_values = new ArrayList<Double>();
-        server_values.add(1.0);
-        server_values.add(2.0);
-        server_values.add(3.0);
+        this.server_values = Collections.synchronizedList(new ArrayList<>());
+        Thread numberGeneratorThread = new Thread(new NumberGenerator(this));
+        numberGeneratorThread.start();
     }
     public void push(PushRequest request, StreamObserver <Empty> responseObserver) {
         List<Double> values = request.getValuesList();
+
         // Process the received list of doubles as needed
-        server_values.addAll(values);
+        for (Double value : values) {
+            if (!server_values.contains(value)) {
+                server_values.add(value);
+            }
+        }
 
         // You can log or perform any other processing here
 
